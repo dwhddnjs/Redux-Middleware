ef9d6adb6e4e827cc75afdc61dc50b3153790a89
dwhddnjs <per1215@naver.com>
2021-07-18 22:23:31 +0900

    add information

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,11 +1,15 @@
+//액션 타입
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
+//액션 생성 함수
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+//초기값
 const initialState = 0;
 
+//리듀서
 export default function couter(state = initialState, action) {
   switch (action.type) {
     case INCREASE:
