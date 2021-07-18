d90cf08be9b65acfd6120e675f98518d6d9ffc5d
dwhddnjs <per1215@naver.com>
2021-07-18 22:24:51 +0900

    add information

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./modules";
 
+//스토어 생성
 const store = createStore(rootReducer);
 
 ReactDOM.render(
+  //프로바이더로 감싸기
   <Provider store={store}>
     <App />
   </Provider>,
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 
+//콤바인 리듀서
 const rootReducer = combineReducers({ counter });
 
 export default rootReducer;
