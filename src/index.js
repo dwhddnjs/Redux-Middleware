c8170b5be980859920395c8b08d812b096be7f69
dwhddnjs <per1215@naver.com>
2021-07-19 22:35:08 +0900

    myLogger from middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
-
+import myLogger from "./middlewares/myLogger";
 //스토어 생성
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 ReactDOM.render(
   //프로바이더로 감싸기
